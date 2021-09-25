8762f826fe7abcb0c12369de45ab2e32fb29b72e
xhdix <xhdix@yahoo.com>
2021-09-25 04:36:38 +0300

    add toggle theme button

--- assets/inject.js
+++ assets/inject.js
@@ -15,3 +15,32 @@ function addCommas( num ) {
 var script = document.createElement('script');
 script.src = 'https://api.github.com/repos/' + BookGitHubUser + '/' + BookGitHubRepo + '?callback=' + callbackName;
 document.head.appendChild( script );
+
+
+// Check for dark mode preference at the OS level
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+// Get the user's theme preference from local storage, if it's available
+const currentTheme = localStorage.getItem("theme");
+// If the user's preference in localStorage is dark...
+if (currentTheme == "dark") {
+  // ...let's toggle the .dark-theme class on the body
+  document.documentElement.classList.toggle("dark");
+// Otherwise, if the user's preference in localStorage is light...
+} else if (currentTheme == "light") {
+  // ...let's toggle the .light-theme class on the body
+  document.documentElement.classList.toggle("light");
+}
+function toggletheme() {
+  if (prefersDarkScheme.matches) {
+    // ...then toggle the light mode class
+    document.documentElement.classList.toggle("light");
+    // ...but use .dark-mode if the .light-mode class is already on the body,
+    var theme = document.documentElement.classList.contains("light") ? "light" : "dark";
+  } else {
+    // Otherwise, let's do the same thing, but for .dark-mode
+    document.documentElement.classList.toggle("dark");
+    var theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
+  }
+  // Finally, let's save the current preference to localStorage to keep using it
+  localStorage.setItem("theme", theme);
+}
