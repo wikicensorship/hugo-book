0e4a230bbd55e469f6b954201102c27e05a08756
xhdix <xhdix@yahoo.com>
2021-09-26 16:38:19 +0300

    improve auto theme

--- assets/inject.js
+++ assets/inject.js
@@ -16,7 +16,9 @@ var script = document.createElement('script');
 script.src = 'https://api.github.com/repos/' + BookGitHubUser + '/' + BookGitHubRepo + '?callback=' + callbackName;
 document.head.appendChild( script );
 
-
+const themeColor = document.querySelector('meta[name="theme-color"]');
+const colorScheme = document.querySelector('meta[name="color-scheme"]');
+const body = document.body;
 // Check for dark mode preference at the OS level
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 // Get the user's theme preference from local storage, if it's available
@@ -24,21 +26,33 @@ const currentTheme = localStorage.getItem("theme");
 // If the user's preference in localStorage is dark...
 if (currentTheme == "dark") {
   // ...let's toggle the .dark-theme class on the body
-  document.documentElement.classList.toggle("dark");
+  if (!prefersDarkScheme.matches) {
+    document.documentElement.classList.toggle("dark");
+  }
+  themeColor.content = getComputedStyle(body).color;
+  colorScheme.content= "dark";
 // Otherwise, if the user's preference in localStorage is light...
 } else if (currentTheme == "light") {
   // ...let's toggle the .light-theme class on the body
-  document.documentElement.classList.toggle("light");
+  if (prefersDarkScheme.matches) {
+    document.documentElement.classList.toggle("light");
+  }
+  themeColor.content = getComputedStyle(body).color;
+  colorScheme.content= "light";
 }
 function toggletheme() {
   if (prefersDarkScheme.matches) {
     // ...then toggle the light mode class
     document.documentElement.classList.toggle("light");
+    themeColor.content = getComputedStyle(body).color;
+    colorScheme.content= colorScheme.content == "dark" ? "light" : "dark";
     // ...but use .dark-mode if the .light-mode class is already on the body,
     var theme = document.documentElement.classList.contains("light") ? "light" : "dark";
   } else {
     // Otherwise, let's do the same thing, but for .dark-mode
     document.documentElement.classList.toggle("dark");
+    themeColor.content = getComputedStyle(body).color;
+    colorScheme.content= colorScheme.content == "dark" ? "light" : "dark";
     var theme = document.documentElement.classList.contains("dark") ? "dark" : "light";
   }
   // Finally, let's save the current preference to localStorage to keep using it
